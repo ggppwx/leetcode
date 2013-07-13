36fe552510c0099e77d581d1e7b0c3970d1e7618
ggppwx <sirgujingwei@gmail.com>
2013-07-13 16:36:17 -0400

    Update level2.4.cpp

--- easyProblems/level2.4.cpp
+++ easyProblems/level2.4.cpp
@@ -3,3 +3,29 @@
 //Return [1,3,3,1].
 //Note:
 //Could you optimize your algorithm to use only O(k) extra space?
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<int> tmp;
+        for (int i = 0; i <= rowIndex; ++i ) {
+            
+            vector<int> A(i+1);
+            for (int j = 0; j <= i ; ++j) {
+                if ( j == 0 || j == i) {
+                    A[j] = 1;
+                }else { 
+                    A[j] = tmp[j-1] + tmp[j];                     
+                }
+                
+            }
+            
+            tmp = A; 
+            
+        }
+        return tmp;
+          
+    }
+};
